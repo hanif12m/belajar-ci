c1bebc57a1a41de5f31116d03d79a89dc95f7bc2
hanif12m <mhanif191202@gmail.com>
2025-07-07 10:36:19 +0700

    final

--- app/Views/v_profile.php
+++ app/Views/v_profile.php
@@ -33,50 +33,45 @@ History Transaksi Pembelian <strong><?= $username ?></strong>
                     </tr>
 
                     <!-- Detail Modal Begin -->
-                    <div class="modal fade" id="detailModal-<?= $item['id'] ?>" tabindex="-1">
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title">Detail Transaksi #<?= $item['id'] ?></h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <?php if (!empty($product[$item['id']])) : ?>
-                                        <?php foreach ($product[$item['id']] as $index2 => $item2) : ?>
-                                            <p><strong><?= $index2 + 1 ?>.</strong></p>
+<div class="modal fade" id="detailModal-<?= $item['id'] ?>" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Detail Transaksi #<?= $item['id'] ?></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <?php if (!empty($product[$item['id']])) : ?>
+          <?php foreach ($product[$item['id']] as $index2 => $item2) : ?>
+            <p><strong><?= $index2 + 1 ?>.</strong></p>
 
-                                            <?php if (!empty($item2['foto']) && file_exists("img/" . $item2['foto'])) : ?>
-                                                <img src="<?= base_url("img/" . $item2['foto']) ?>" width="100px"><br>
-                                            <?php endif; ?>
+            <?php if (!empty($item2['foto']) && file_exists("img/" . $item2['foto'])) : ?>
+              <img src="<?= base_url("img/" . $item2['foto']) ?>" width="100px"><br>
+            <?php endif; ?>
 
-                                            <strong><?= $item2['nama'] ?></strong><br>
-                                            
-                                            <!-- Harga Asli -->
-                                            <small class="text-muted">
-                                                Harga Asli: <s><?= number_to_currency($item2['harga'] + $item2['diskon'], 'IDR') ?></s>
-                                            </small><br>
+            <strong><?= $item2['nama'] ?></strong><br>
 
-                                            <!-- Harga Setelah Diskon -->
-                                            Harga Setelah Diskon: <strong><?= number_to_currency($item2['harga'], 'IDR') ?></strong><br>
+            <!-- Harga Asli -->
+            Harga Asli: <strong><?= number_to_currency($item2['harga'] + $item2['diskon'], 'IDR') ?></strong><br>
 
-                                            <!-- Diskon -->
-                                            <?php if ($item2['diskon'] > 0): ?>
-                                                Diskon: -<?= number_to_currency($item2['diskon'], 'IDR') ?><br>
-                                            <?php endif; ?>
+            <!-- Harga Setelah Diskon -->
+            Harga Setelah Diskon: <strong><?= number_to_currency($item2['harga'], 'IDR') ?></strong><br>
 
-                                            Jumlah: <?= $item2['jumlah'] ?> pcs<br>
-                                            Subtotal: <strong><?= number_to_currency($item2['subtotal_harga'], 'IDR') ?></strong>
-                                            <hr>
-                                        <?php endforeach; ?>
-                                    <?php else : ?>
-                                        <p>Tidak ada detail produk.</p>
-                                    <?php endif; ?>
-                                    Ongkir: <strong><?= number_to_currency($item['ongkir'], 'IDR') ?></strong>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Detail Modal End -->
+            <!-- Jumlah Item -->
+            Jumlah: <?= $item2['jumlah'] ?> pcs<br>
+
+            <hr>
+          <?php endforeach; ?>
+        <?php else : ?>
+          <p>Tidak ada detail produk.</p>
+        <?php endif; ?>
+        Ongkir: <strong><?= number_to_currency($item['ongkir'], 'IDR') ?></strong>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Detail Modal End -->
+
                 <?php endforeach; ?>
             <?php endif; ?>
         </tbody>
